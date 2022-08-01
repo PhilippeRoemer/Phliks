33c1644f81220530168a66508ba2e35fbf7f26ee
Philippe Roemer <philipperoemer@yahoo.com>
2022-08-01 08:42:56 -0400

    Movie info container is hidden if no movie is selected

--- src/Pages/Random.js
+++ src/Pages/Random.js
@@ -72,56 +72,64 @@ function Random() {
 
     return (
         <div>
-            <div
-                className="infoContainerBackground"
-                style={{
-                    backgroundImage: `linear-gradient(rgba(187, 187, 187, 0.52), rgb(0, 0, 0)),url(${"https://www.themoviedb.org/t/p/w1920_and_h800_multi_faces/" + movieBackdrop})`,
-                }}
-            >
-                {" "}
-                <h1>TESTING</h1>
-                <div onClick={randomMovie}>Click here!</div>
-                <div className="infoContainer">
-                    <img className="infoContainerPoster" src={"http://image.tmdb.org/t/p/w500" + moviePoster} alt="" />
-                    <div>
-                        <h1>{movieTitle}</h1>
-                        <br />
-                        <p>
-                            <b>Description:</b> {movieDescription}
-                        </p>
-                        <br />
-                        <p>
-                            <b>Released:</b> {movieYear}
-                        </p>
-                        <br />
-                        <p>
-                            <b>Rating:</b> {movieRating}
-                        </p>
-                        <br />
-                        {movieTrailer === null ? null : (
-                            <button className="button">
-                                <a href={"https://www.youtube.com/watch?v=" + movieTrailer} target="_blank">
-                                    Watch Trailer
-                                </a>
-                            </button>
-                        )}
+            {movieTitle === null ? (
+                <>
+                    <div className="randomMovieButtonContainer">
+                        <div onClick={randomMovie}>Click here for a random movie</div>
                     </div>
-                </div>
-            </div>
-            <div className="detailContainer">
-                <h1 className="crewTitle">Cast & Crew</h1>
-                <div className="crewContainer">
-                    {movieCast.slice(0, 8).map((cast) => {
-                        return (
-                            <div className="crew">
-                                <img src={cast.profile_path !== null ? "https://www.themoviedb.org/t/p/w138_and_h175_face/" + cast.profile_path : noPicture} alt="" className="crewImage" />
-                                <p className="castName">{cast.name}</p>
-                                <p className="castCharacter">{cast.character}</p>
+                </>
+            ) : (
+                <>
+                    <div
+                        className="infoContainerBackground"
+                        style={{
+                            backgroundImage: `linear-gradient(rgba(187, 187, 187, 0.52), rgb(0, 0, 0)),url(${"https://www.themoviedb.org/t/p/w1920_and_h800_multi_faces/" + movieBackdrop})`,
+                        }}
+                    >
+                        <div onClick={randomMovie}>New Random Movie</div>
+                        <div className="infoContainer">
+                            <img className="infoContainerPoster" src={"http://image.tmdb.org/t/p/w500" + moviePoster} alt="" />
+                            <div>
+                                <h1>{movieTitle}</h1>
+                                <br />
+                                <p>
+                                    <b>Description:</b> {movieDescription}
+                                </p>
+                                <br />
+                                <p>
+                                    <b>Released:</b> {movieYear}
+                                </p>
+                                <br />
+                                <p>
+                                    <b>Rating:</b> {movieRating}
+                                </p>
+                                <br />
+                                {movieTrailer === null ? null : (
+                                    <button className="button">
+                                        <a href={"https://www.youtube.com/watch?v=" + movieTrailer} target="_blank">
+                                            Watch Trailer
+                                        </a>
+                                    </button>
+                                )}
                             </div>
-                        );
-                    })}
-                </div>
-            </div>
+                        </div>
+                    </div>
+                    <div className="detailContainer">
+                        <h1 className="crewTitle">Cast & Crew</h1>
+                        <div className="crewContainer">
+                            {movieCast.slice(0, 8).map((cast) => {
+                                return (
+                                    <div className="crew">
+                                        <img src={cast.profile_path !== null ? "https://www.themoviedb.org/t/p/w138_and_h175_face/" + cast.profile_path : noPicture} alt="" className="crewImage" />
+                                        <p className="castName">{cast.name}</p>
+                                        <p className="castCharacter">{cast.character}</p>
+                                    </div>
+                                );
+                            })}
+                        </div>
+                    </div>
+                </>
+            )}
         </div>
     );
 }
