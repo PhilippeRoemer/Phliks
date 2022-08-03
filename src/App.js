d62891610ce1eaa96c0a840b01ef99e0022ea469
Philippe Roemer <philipperoemer@yahoo.com>
2022-08-03 05:53:52 -0400

    App routes updated

--- src/App.js
+++ src/App.js
@@ -2,20 +2,25 @@ import "./App.css";
 import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
 import Home from "./Pages/Home";
 import Movie from "./Pages/Movie";
+import TV from "./Pages/TV";
+import Random from "./Pages/Random";
+import Search from "./Pages/Search";
 
 function App() {
     return (
         <Router>
             <div className="App">
-                <nav>
+                <div className="nav">
                     <Link to="/">Home</Link>
                     <Link to="/random">Random Movie</Link>
                     <Link to="/search">Search</Link>
-                </nav>
-
+                </div>
                 <Routes>
                     <Route path="/" element={<Home />} />
                     <Route path="/movie/:id" element={<Movie />} />
+                    <Route path="/tv/:id" element={<TV />} />
+                    <Route path="/random" element={<Random />} />
+                    <Route path="/search" element={<Search />} />
                 </Routes>
             </div>
         </Router>
